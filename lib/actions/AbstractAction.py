709b3fbf999358a3ca9665cefc23dfba5623369e
Nathan <nembery@gmail.com>
2018-12-05 11:04:56 -0600

    cleanups on Docker actions

--- lib/actions/AbstractAction.py
+++ lib/actions/AbstractAction.py
@@ -1,3 +1,6 @@
+# base class to build reusable utility actions. Actions are bits of code that perform some action usually against
+# an API or some executable. The idea here is to make this as simple and abstract as possible
+
 from abc import ABC
 
 
@@ -7,51 +10,20 @@ class AbstractAction(ABC):
     @abc.abstractmethod
     def execute_template(self, template):
         """
-        :param template: thh configuration template to be executed from the input_form
+         execute the action with the compiled template as an input
+        :param template: compiled template
+        :return: string
+        """
+        return
+
+    @abc.abstractmethod
+    def cleanup(self):
+        """
+        Perform any cleanup actions needed
         :return:
         """
         return
 
-    def get_config_options(self):
-        return []
 
-    # def get_default_config(self):
-    #     default_config = dict()
-    #     for item in self.get_config_options():
-    #         default_config[item['name']] = item['default']
-    #
-    #     return ActionConfig(default_config)
-
-    def set_global_options(self, data):
-        """
-        :param data: class data from the settings.py file if configured
-        :return:
-        """
-
-        for member in data:
-            if hasattr(self, member):
-                setattr(self, member, str(data[member]))
-
-        return
-
-    def set_instance_options(self, data):
-        """
-        :param data: instance data from the admin configured template file
-        :return: None
-        """
-        for member in data:
-            if hasattr(self, member):
-                setattr(self, member, data[member]["value"])
-
-        return
-
-    @staticmethod
-    def continue_workflow(self, can_continue, exit_message):
-        """
-        :param can_continue: boolean - should workflow continue? Similar to stderr in Unix pipes
-        :param exit_message: String - Similar to stdout
-        :return:
-        """
-        return {"continue": can_continue, "message": exit_message}
 
 
